f0f88deb714595eaf34e0808dda448de6000b9e8
fasil <fasil.marshooq@agoda.com>
2024-02-06 22:13:09 +0700

    added nullable phone number field

--- MongoDbCrudApp/Data/Student.cs
+++ MongoDbCrudApp/Data/Student.cs
@@ -7,6 +7,6 @@
         public string Name { get; set; }
         public string Department { get; set; }
         public string Email { get; set; }
-
+        public string Phone { get; set; }
     }
 }
